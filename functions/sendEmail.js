973525b4f26e23d04cde31305f0da959bd3cbb8b
hamza <elbazhamza77@gmail.com>
2026-02-16 20:14:57 +0100

    Initial commit

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -13,7 +13,7 @@ exports.handler = async function (event, context) {
             service: 'gmail',
             auth: {
                 user: 'elbazhamzahb@gmail.com',
-                pass: 'sktv qkhi yagb cnxg',
+                pass: 'dgmi mkqx jwzy ptrt',
             },
         });
 
